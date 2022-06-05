bccadb682826572bf70b92dcf090948449b9ff46
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-06 01:03:11 +0500

    push

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.on('message', async (ctx) => {
+    try {
+        ctx.replyWithLocation(0, 0);
+    } catch (err) {
+        console.log(err);
+        ctx.reply('Botda xatolik roy berdi', {
+            reply_to_message_id: ctx.message.message_id,
+        });
+    }
+});
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 bot.launch();
