b02364b87b651ed1bf349a0acc92197c85abff2c
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-07 23:05:51 +0500

    try catch removed

--- modules/youtube.js
+++ modules/youtube.js
@@ -1,43 +1,36 @@
 const SendVideo = async (contex, ytdl) => {
-    try {
-        contex.reply('⏳');
-        const info = await ytdl.getInfo(contex.message.text);
-        if (info.formats[0].contentLength / 1024 / 1024 < 30) {
-            const stream = ytdl(contex.message.text);
-            contex.replyWithVideo(
-                {
-                    source: stream,
-                },
-                {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: 'Audio',
-                                    callback_data: contex.message.text,
-                                },
-                            ],
+    contex.reply('⏳');
+    const info = await ytdl.getInfo(contex.message.text);
+    if (info.formats[0].contentLength / 1024 / 1024 < 30) {
+        const stream = ytdl(contex.message.text);
+        contex.replyWithVideo(
+            {
+                source: stream,
+            },
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: 'Audio',
+                                callback_data: contex.message.text,
+                            },
                         ],
-                    },
-                    parse_mode: 'HTML',
-                    caption: `
+                    ],
+                },
+                parse_mode: 'HTML',
+                caption: `
                             @forLearningro_bot\n\n<i>${info.videoDetails.title}</i>
                             `,
-                }
-            );
-        } else {
-            contex.reply(
-                'Ushbu videoni hajmi kutilgandan kattaroq iltimos faqat kichik hajmdagi videolarni yuboring !!!',
-                {
-                    reply_to_message_id: contex.message.message_id,
-                }
-            );
-        }
-    } catch (err) {
-        console.log(err);
-        contex.reply('Bu turdagi videni yuklab bolmaydi i`m sorry !!!', {
-            reply_to_message_id: contex.message.message_id,
-        });
+            }
+        );
+    } else {
+        contex.reply(
+            'Ushbu videoni hajmi kutilgandan kattaroq iltimos faqat kichik hajmdagi videolarni yuboring !!!',
+            {
+                reply_to_message_id: contex.message.message_id,
+            }
+        );
     }
 };
 
