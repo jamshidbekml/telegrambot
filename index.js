6fd7a88570352aeb0e91349a54fe9c8dea5f951f
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-07 15:58:44 +0500

    push

--- index.js
+++ index.js
@@ -11,29 +11,38 @@ bot.on('message', async (ctx) => {
     try {
         if (ytdl.validateURL(ctx.message.text)) {
             ctx.reply('⏳');
-            const stream = ytdl(ctx.message.text);
             const info = await ytdl.getInfo(ctx.message.text);
-            ctx.replyWithVideo(
-                {
-                    source: stream,
-                },
-                {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [
-                                {
-                                    text: 'Audio',
-                                    callback_data: ctx.message.text,
-                                },
-                            ],
-                        ],
+            if (info.formats[0].contentLength / 1024 / 1024 < 30) {
+                const stream = ytdl(ctx.message.text);
+                ctx.replyWithVideo(
+                    {
+                        source: stream,
                     },
-                    parse_mode: 'HTML',
-                    caption: `
-                    @forLearningro_bot\n\n<i>${info.videoDetails.title}</i>
-                    `,
-                }
-            );
+                    {
+                        reply_markup: {
+                            inline_keyboard: [
+                                [
+                                    {
+                                        text: 'Audio',
+                                        callback_data: ctx.message.text,
+                                    },
+                                ],
+                            ],
+                        },
+                        parse_mode: 'HTML',
+                        caption: `
+                        @forLearningro_bot\n\n<i>${info.videoDetails.title}</i>
+                        `,
+                    }
+                );
+            } else {
+                ctx.reply(
+                    'Ushbu videoni hajmi kutilgandan kattaroq iltimos faqat kichik hajmdagi videolarni yuboring !!!',
+                    {
+                        reply_to_message_id: ctx.message.message_id,
+                    }
+                );
+            }
         } else {
             ctx.reply('Ushbu link xato kiritilgan', {
                 reply_to_message_id: ctx.message.message_id,
