3807395ef6738165db3f32e236ecb6529f76754b
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-17 16:42:28 +0500

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
     CountCommand,
     QueryListener,
 } = require('./lib/commands');
-const { sequelize } = require('./model/model');
+const { sequelize, BotSubscribers } = require('./model/model');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -14,6 +14,19 @@ sequelize
     .sync({ force: false })
     .then(() => console.log('connected'))
     .catch((err) => console.log(err));
+
+// const UserMesages = async (msg) => {
+//     try {
+//         const users = await BotSubscribers.findAll();
+//         users.forEach((e) => {
+//             ctx.telegram.sendMessage(1771234776, msg);
+//         });
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// };
+
+// UserMesages('Botga instagramdan videolar tortish funksiyasi qo`shildi');
 
 bot.start((ctx) => StartCommand(ctx));
 
