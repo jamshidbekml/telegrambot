580685d61fbb1625647682f22c4623c25b9b088e
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-22 23:10:36 +0500

    push

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-const InstagramVideo = async (ctx, story) => {
+const InstagramVideo = async (ctx) => {
     try {
         ctx.reply('⏳');
         const { data } = await axios.post(
@@ -40,88 +40,6 @@ const InstagramVideo = async (ctx, story) => {
                 },
             ]);
         }
-        // ctx.replyWithVideo(
-        //     {
-        //         url: link,
-        //     },
-        //     {
-        //         caption: `@smvideosdl_bot`,
-        //     }
-        // );
-        // if (
-        //     JSON.parse(data).videos_links.length == 0 &&
-        //     JSON.parse(data).images_links.length == 0
-        // ) {
-        //     const newLinkArr = ctx.message.text.split('/');
-        //     const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
-        //     const { data } = await axios.get(
-        //         'https://instadownloader.co/insta_downloader.php?url=' +
-        //             newLink,
-        //         {
-        //             headers: {
-        //                 'User-Agent':
-        //                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-        //             },
-        //         }
-        //     );
-        //     if (JSON.parse(data).images_links && story) {
-        //         const ImagesLinks = JSON.parse(data).images_links;
-        //         ImagesLinks.forEach((e) => {
-        //             ctx.replyWithChatAction('upload_photo');
-        //             ctx.replyWithPhoto(
-        //                 {
-        //                     url: e.url,
-        //                 },
-        //                 {
-        //                     caption: `@smvideosdl_bot`,
-        //                 }
-        //             );
-        //         });
-        //     }
-        //     if (JSON.parse(data).videos_links) {
-        //         const VideoLinks = JSON.parse(data).videos_links;
-        //         VideoLinks.forEach(async (e) => {
-        //             ctx.replyWithChatAction('upload_video');
-        //             ctx.replyWithVideo(
-        //                 {
-        //                     url: e.url,
-        //                 },
-        //                 {
-        //                     caption: `@smvideosdl_bot`,
-        //                 }
-        //             );
-        //         });
-        //     }
-        //     return;
-        // }
-        // if (JSON.parse(data).images_links && story) {
-        //     const ImagesLinks = JSON.parse(data).images_links;
-        //     ImagesLinks.forEach((e) => {
-        //         ctx.replyWithChatAction('upload_photo');
-        //         ctx.replyWithPhoto(
-        //             {
-        //                 url: e.url,
-        //             },
-        //             {
-        //                 caption: `@smvideosdl_bot`,
-        //             }
-        //         );
-        //     });
-        // }
-        // if (JSON.parse(data).videos_links) {
-        //     const VideoLinks = JSON.parse(data).videos_links;
-        //     VideoLinks.forEach(async (e) => {
-        //         ctx.replyWithChatAction('upload_video');
-        //         ctx.replyWithVideo(
-        //             {
-        //                 url: e.url,
-        //             },
-        //             {
-        //                 caption: `@smvideosdl_bot`,
-        //             }
-        //         );
-        //     });
-        // }
     } catch (err) {
         ctx.reply(
             'This link is incorrectly entered or the user account is private',
@@ -132,4 +50,75 @@ const InstagramVideo = async (ctx, story) => {
         console.log(err.message);
     }
 };
-module.exports = { InstagramVideo };
+
+const InstagramStories = async (ctx) => {
+    try {
+        ctx.reply('⏳');
+        const { data } = await axios.get(
+            'https://instadownloader.co/insta_downloader.php?url=' +
+                ctx.message.text,
+            {
+                headers: {
+                    'User-Agent':
+                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                },
+            }
+        );
+        var newData = JSON.parse(data);
+        if (
+            newData.videos_links.length == 0 &&
+            newData.images_links.length == 0
+        ) {
+            const newLinkArr = ctx.message.text.split('/');
+            const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
+            const { data } = await axios.get(
+                'https://instadownloader.co/insta_downloader.php?url=' +
+                    newLink,
+                {
+                    headers: {
+                        'User-Agent':
+                            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                    },
+                }
+            );
+            newData = JSON.parse(data);
+        }
+        if (newData.images_links) {
+            const ImagesLinks = newData.images_links;
+            ImagesLinks.forEach((e) => {
+                ctx.replyWithChatAction('upload_photo');
+                ctx.replyWithPhoto(
+                    {
+                        url: e.url,
+                    },
+                    {
+                        caption: `@smvideosdl_bot`,
+                    }
+                );
+            });
+        }
+        if (newData.videos_links) {
+            const VideoLinks = newData.videos_links;
+            VideoLinks.forEach(async (e) => {
+                ctx.replyWithChatAction('upload_video');
+                ctx.replyWithVideo(
+                    {
+                        url: e.url,
+                    },
+                    {
+                        caption: `@smvideosdl_bot`,
+                    }
+                );
+            });
+        }
+    } catch (err) {
+        console.log(err.message);
+        ctx.reply(
+            'This link is incorrectly entered or the user account is private',
+            {
+                reply_to_message_id: ctx.message.message_id,
+            }
+        );
+    }
+};
+module.exports = { InstagramVideo, InstagramStories };
