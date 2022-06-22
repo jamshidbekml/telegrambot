601ac01b7e20fe83e6c664b10e354a890bc675d3
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-23 02:45:21 +0500

    push

--- modules/instagram.js
+++ modules/instagram.js
@@ -3,11 +3,9 @@ const axios = require('axios').default;
 const InstagramVideo = async (ctx) => {
     try {
         ctx.reply('⏳');
-        const { data } = await axios.post(
-            'https://storiesig.info/api/convert',
-            {
-                url: ctx.message.text,
-            },
+        const { data } = await axios.get(
+            'https://instadownloader.co/insta_downloader.php?url=' +
+                ctx.message.text,
             {
                 headers: {
                     'User-Agent':
@@ -15,49 +13,95 @@ const InstagramVideo = async (ctx) => {
                 },
             }
         );
-        var type;
-        var url;
-        if (data[0]) {
-            data.forEach((e) => {
-                type = e.url[0].type == 'mp4' ? 'video' : 'photo';
-                url = e.url[0].url;
-                ctx.replyWithChatAction(`upload_${type}`);
-                ctx.telegram
-                    .sendMediaGroup(ctx.message.chat.id, [
-                        {
-                            type: type,
-                            media: url,
-                            caption: `@smvideosdl_bot`,
-                        },
-                    ])
-                    .then((res) => {
-                        ctx.telegram.forwardMessage(
-                            '@downloadedVideos',
-                            res[0].chat.id,
-                            res[0].message_id
-                        );
-                    });
-            });
-        } else {
-            type = data.url[0].type == 'mp4' ? 'video' : 'photo';
-            url = data.url[0].url;
-            ctx.replyWithChatAction(`upload_${type}`);
-            ctx.telegram
-                .sendMediaGroup(ctx.message.chat.id, [
-                    {
-                        type: type,
-                        media: url,
-                        caption: `@smvideosdl_bot`,
+        var newData = JSON.parse(data);
+        if (
+            newData.videos_links.length == 0 &&
+            newData.images_links.length == 0
+        ) {
+            const newLinkArr = ctx.message.text.split('/');
+            const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
+            const { data } = await axios.get(
+                'https://instadownloader.co/insta_downloader.php?url=' +
+                    newLink,
+                {
+                    headers: {
+                        'User-Agent':
+                            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
                     },
-                ])
-                .then((res) => {
+                }
+            );
+            newData = JSON.parse(data);
+        }
+        if (newData.images_links) {
+            const ImagesLinks = newData.images_links;
+            ImagesLinks.forEach((e) => {
+                ctx.replyWithChatAction('upload_photo');
+                ctx.replyWithPhoto(
+                    {
+                        url: e.url,
+                    },
+                    {
+                        caption: `@smvideosdl_bot`,
+                    }
+                ).then((res) => {
                     ctx.telegram.forwardMessage(
                         '@downloadedVideos',
-                        res[0].chat.id,
-                        res[0].message_id
+                        res.chat.id,
+                        res.message_id
                     );
                 });
+            });
         }
+        if (newData.videos_links) {
+            const VideoLinks = newData.videos_links;
+            VideoLinks.forEach(async (e) => {
+                ctx.replyWithChatAction('upload_video');
+                ctx.replyWithVideo(
+                    {
+                        url: e.url,
+                    },
+                    {
+                        caption: `@smvideosdl_bot`,
+                    }
+                ).then((res) => {
+                    ctx.telegram.forwardMessage(
+                        '@downloadedVideos',
+                        res.chat.id,
+                        res.message_id
+                    );
+                });
+            });
+        }
+        // const { data } = await axios.post(
+        //     'https://storiesig.info/api/convert',
+        //     {
+        //         url: ctx.message.text,
+        //     },
+        //     {
+        //         headers: {
+        //             'User-Agent':
+        //                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        //         },
+        //     }
+        // );
+        // const type = data.url[0].type == 'mp4' ? 'video' : 'photo';
+        // const url = data.url[0].url;
+        // ctx.replyWithChatAction(`upload_${type}`);
+        // ctx.telegram
+        //     .sendMediaGroup(ctx.message.chat.id, [
+        //         {
+        //             type: type,
+        //             media: url,
+        //             caption: `@smvideosdl_bot`,
+        //         },
+        //     ])
+        //     .then((res) => {
+        //         ctx.telegram.forwardMessage(
+        //             '@downloadedVideos',
+        //             res[0].chat.id,
+        //             res[0].message_id
+        //         );
+        //     });
     } catch (err) {
         ctx.reply(
             'This link is incorrectly entered or the user account is private',
