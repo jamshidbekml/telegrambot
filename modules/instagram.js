1d99ff6ca534c2ff033fcd96a739d119eb550090
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-22 15:11:48 +0500

    push

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,11 +1,13 @@
 const axios = require('axios').default;
 
-const Instagram = async (ctx, story) => {
+const InstagramVideo = async (ctx, story) => {
     try {
         ctx.reply('⏳');
-        const { data } = await axios.get(
-            'https://instadownloader.co/insta_downloader.php?url=' +
-                ctx.message.text,
+        const { data } = await axios.post(
+            'https://storiesig.info/api/convert',
+            {
+                url: ctx.message.text,
+            },
             {
                 headers: {
                     'User-Agent':
@@ -13,80 +15,90 @@ const Instagram = async (ctx, story) => {
                 },
             }
         );
-        if (
-            JSON.parse(data).videos_links.length == 0 &&
-            JSON.parse(data).images_links.length == 0
-        ) {
-            const newLinkArr = ctx.message.text.split('/');
-            const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
-            const { data } = await axios.get(
-                'https://instadownloader.co/insta_downloader.php?url=' +
-                    newLink,
-                {
-                    headers: {
-                        'User-Agent':
-                            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-                    },
-                }
-            );
-            if (JSON.parse(data).images_links && story) {
-                const ImagesLinks = JSON.parse(data).images_links;
-                ImagesLinks.forEach((e) => {
-                    ctx.replyWithChatAction('upload_photo');
-                    ctx.replyWithPhoto(
-                        {
-                            url: e.url,
-                        },
-                        {
-                            caption: `@smvideosdl_bot`,
-                        }
-                    );
-                });
+        const link = data[0].url[0].url;
+        ctx.replyWithChatAction('upload_video');
+        ctx.replyWithVideo(
+            {
+                url: link,
+            },
+            {
+                caption: `@smvideosdl_bot`,
             }
-            if (JSON.parse(data).videos_links) {
-                const VideoLinks = JSON.parse(data).videos_links;
-                VideoLinks.forEach(async (e) => {
-                    ctx.replyWithChatAction('upload_video');
-                    ctx.replyWithVideo(
-                        {
-                            url: e.url,
-                        },
-                        {
-                            caption: `@smvideosdl_bot`,
-                        }
-                    );
-                });
-            }
-            return;
-        }
-        if (JSON.parse(data).images_links && story) {
-            const ImagesLinks = JSON.parse(data).images_links;
-            ImagesLinks.forEach((e) => {
-                ctx.replyWithChatAction('upload_photo');
-                ctx.replyWithPhoto(
-                    {
-                        url: e.url,
-                    },
-                    {
-                        caption: `@smvideosdl_bot`,
-                    }
-                );
-            });
-        }
-        if (JSON.parse(data).videos_links) {
-            const VideoLinks = JSON.parse(data).videos_links;
-            VideoLinks.forEach(async (e) => {
-                ctx.replyWithChatAction('upload_video');
-                ctx.replyWithVideo(
-                    {
-                        url: e.url,
-                    },
-                    {
-                        caption: `@smvideosdl_bot`,
-                    }
-                );
-            });
-        }
+        );
+        // if (
+        //     JSON.parse(data).videos_links.length == 0 &&
+        //     JSON.parse(data).images_links.length == 0
+        // ) {
+        //     const newLinkArr = ctx.message.text.split('/');
+        //     const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
+        //     const { data } = await axios.get(
+        //         'https://instadownloader.co/insta_downloader.php?url=' +
+        //             newLink,
+        //         {
+        //             headers: {
+        //                 'User-Agent':
+        //                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        //             },
+        //         }
+        //     );
+        //     if (JSON.parse(data).images_links && story) {
+        //         const ImagesLinks = JSON.parse(data).images_links;
+        //         ImagesLinks.forEach((e) => {
+        //             ctx.replyWithChatAction('upload_photo');
+        //             ctx.replyWithPhoto(
+        //                 {
+        //                     url: e.url,
+        //                 },
+        //                 {
+        //                     caption: `@smvideosdl_bot`,
+        //                 }
+        //             );
+        //         });
+        //     }
+        //     if (JSON.parse(data).videos_links) {
+        //         const VideoLinks = JSON.parse(data).videos_links;
+        //         VideoLinks.forEach(async (e) => {
+        //             ctx.replyWithChatAction('upload_video');
+        //             ctx.replyWithVideo(
+        //                 {
+        //                     url: e.url,
+        //                 },
+        //                 {
+        //                     caption: `@smvideosdl_bot`,
+        //                 }
+        //             );
+        //         });
+        //     }
+        //     return;
+        // }
+        // if (JSON.parse(data).images_links && story) {
+        //     const ImagesLinks = JSON.parse(data).images_links;
+        //     ImagesLinks.forEach((e) => {
+        //         ctx.replyWithChatAction('upload_photo');
+        //         ctx.replyWithPhoto(
+        //             {
+        //                 url: e.url,
+        //             },
+        //             {
+        //                 caption: `@smvideosdl_bot`,
+        //             }
+        //         );
+        //     });
+        // }
+        // if (JSON.parse(data).videos_links) {
+        //     const VideoLinks = JSON.parse(data).videos_links;
+        //     VideoLinks.forEach(async (e) => {
+        //         ctx.replyWithChatAction('upload_video');
+        //         ctx.replyWithVideo(
+        //             {
+        //                 url: e.url,
+        //             },
+        //             {
+        //                 caption: `@smvideosdl_bot`,
+        //             }
+        //         );
+        //     });
+        // }
     } catch (err) {
         ctx.reply(
             'This link is incorrectly entered or the user account is private',
@@ -97,4 +109,4 @@ const Instagram = async (ctx, story) => {
         console.log(err.message);
     }
 };
-module.exports = { Instagram };
+module.exports = { InstagramVideo };
