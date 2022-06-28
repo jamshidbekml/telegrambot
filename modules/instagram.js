b987f1c5c82ffd6c977ab3d16aca97cb7835c833
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-29 00:43:14 +0500

    push

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const { UrlIsValid } = require('../lib/urlIsValid');
 
 const InstagramVideo = async (ctx) => {
     try {
@@ -18,6 +19,7 @@ const InstagramVideo = async (ctx) => {
             newData.videos_links.length == 0 &&
             newData.images_links.length == 0
         ) {
+            console.log('Qayta murojat');
             const newLinkArr = ctx.message.text.split('/');
             const newLink = `https://www.instagram.com/${newLinkArr[3]}/${newLinkArr[4]}`;
             const { data } = await axios.get(
@@ -36,40 +38,75 @@ const InstagramVideo = async (ctx) => {
             const ImagesLinks = newData.images_links;
             ImagesLinks.forEach((e) => {
                 ctx.replyWithChatAction('upload_photo');
-                ctx.replyWithPhoto(
-                    {
-                        url: e.url,
-                    },
-                    {
-                        caption: `@smvideosdl_bot`,
-                    }
-                ).then((res) => {
-                    ctx.telegram.forwardMessage(
-                        '@downloadedVideos',
-                        res.chat.id,
-                        res.message_id
-                    );
-                });
+                ctx.telegram
+                    .sendMediaGroup(ctx.message.chat.id, [
+                        {
+                            type: 'photo',
+                            media: e.url,
+                            caption: `@smvideosdl_bot`,
+                        },
+                    ])
+                    .then((res) => {
+                        ctx.telegram.forwardMessage(
+                            '@downloadedVideos',
+                            res[0].chat.id,
+                            res[0].message_id
+                        );
+                    });
             });
         }
         if (newData.videos_links) {
             const VideoLinks = newData.videos_links;
             VideoLinks.forEach(async (e) => {
-                ctx.replyWithChatAction('upload_video');
-                ctx.replyWithVideo(
-                    {
-                        url: e.url,
-                    },
-                    {
-                        caption: `@smvideosdl_bot`,
-                    }
-                ).then((res) => {
-                    ctx.telegram.forwardMessage(
-                        '@downloadedVideos',
-                        res.chat.id,
-                        res.message_id
+                if (UrlIsValid(e.url)) {
+                    ctx.reply('Please wait a little longer');
+                    const { data } = await axios.post(
+                        'https://storiesig.info/api/convert',
+                        {
+                            url: ctx.message.text,
+                        },
+                        {
+                            headers: {
+                                'User-Agent':
+                                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                            },
+                        }
                     );
-                });
+                    const type = data.url[0].type == 'mp4' ? 'video' : 'photo';
+                    const url = data.url[0].url;
+                    ctx.replyWithChatAction(`upload_${type}`);
+                    ctx.telegram
+                        .sendMediaGroup(ctx.message.chat.id, [
+                            {
+                                type: type,
+                                media: url,
+                                caption: `@smvideosdl_bot`,
+                            },
+                        ])
+                        .then((res) => {
+                            ctx.telegram.forwardMessage(
+                                '@downloadedVideos',
+                                res[0].chat.id,
+                                res[0].message_id
+                            );
+                        });
+                } else {
+                    ctx.replyWithChatAction('upload_video');
+                    ctx.replyWithVideo(
+                        {
+                            url: e.url,
+                        },
+                        {
+                            caption: `@smvideosdl_bot`,
+                        }
+                    ).then((res) => {
+                        ctx.telegram.forwardMessage(
+                            '@downloadedVideos',
+                            res.chat.id,
+                            res.message_id
+                        );
+                    });
+                }
             });
         }
         // const { data } = await axios.post(
@@ -149,20 +186,21 @@ const InstagramStories = async (ctx) => {
             const ImagesLinks = newData.images_links;
             ImagesLinks.forEach((e) => {
                 ctx.replyWithChatAction('upload_photo');
-                ctx.replyWithPhoto(
-                    {
-                        url: e.url,
-                    },
-                    {
-                        caption: `@smvideosdl_bot`,
-                    }
-                ).then((res) => {
-                    ctx.telegram.forwardMessage(
-                        '@downloadedVideos',
-                        res.chat.id,
-                        res.message_id
-                    );
-                });
+                ctx.telegram
+                    .sendMediaGroup(ctx.message.chat.id, [
+                        {
+                            type: 'photo',
+                            media: e.url,
+                            caption: `@smvideosdl_bot`,
+                        },
+                    ])
+                    .then((res) => {
+                        ctx.telegram.forwardMessage(
+                            '@downloadedVideos',
+                            res[0].chat.id,
+                            res[0].message_id
+                        );
+                    });
             });
         }
         if (newData.videos_links) {
