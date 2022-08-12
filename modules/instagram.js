a408ff9f38327c2ed765b51e7284272d4ba4d5b9
JamshidbekJS <jamshidbekml@gmail.com>
2022-08-12 17:58:13 +0500

    push

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,68 +1,44 @@
+const ipp = require('instagram-profile-picture')
+
 require('dotenv').config();
 const axios = require('axios').default;
 
 const InstagramVideo = async (ctx) => {
     try {
         ctx.reply('⏳');
-        const {data} = await axios.post(
-            'https://reelit.io/api/fetch',
-            {
-                url: ctx.message.text,
-            },
-            {
-                headers: {
-                    'User-Agent':
-                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-                        'X-Requested-With': 'XMLHttpRequest'
-                },
+        const {data} = await axios.post('https://instasupersave.com/api/convert',{
+            url: ctx.message.text
+        },{
+            headers:{
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest',
+                url: ctx.message.text
             }
-        );
+        })
 
-        const links = data.media.data.mediaList
-        links.forEach((e) => {
-            var videos = e.videos ? e.videos : []
-            var images = e.images ? e.images : []
-            var username = e.user ? e.user.username : `Instagramdan ko'rish`
-            if(videos.length){
-                ctx.replyWithChatAction(`upload_video`);
-                ctx.telegram
-                    .sendMediaGroup(ctx.message.chat.id, [
-                        {
-                            type: 'video',
-                            media: videos[0].url,
-                            parse_mode: 'HTML',
-                            caption: `
-                            @smvideosdl_bot\n\n<i><a href="${ctx.message.text}">${username}</a></i>
-                            `,
-                        },
-                    ])
-                    .then((res) => {
-                        ctx.telegram.forwardMessage(
-                            '@downloadedVideos',
-                            res[0].chat.id,
-                            res[0].message_id
-                        );
-                    });
-            } else if(!videos.length && images) {
-                console.log(e);
-                ctx.replyWithChatAction(`upload_photo`);
-                ctx.telegram
-                    .sendMediaGroup(ctx.message.chat.id, [
-                        {
-                            type: 'photo',
-                            media: e.images[2].url,
-                            caption: `@smvideosdl_bot`,
-                        },
-                    ])
-                    .then((res) => {
-                        ctx.telegram.forwardMessage(
-                            '@downloadedVideos',
-                            res[0].chat.id,
-                            res[0].message_id
-                        );
-                    });
-            }
-        });
+        var newData = data.url ? data.url : data
+        newData.forEach(e => {
+            var createUrl = Array.isArray(e.url) ? e.url[0] : e
+            var type = createUrl.type === 'jpg' ? 'photo' : 'video'
+            ctx.replyWithChatAction(`upload_${type}`);
+            ctx.telegram
+                .sendMediaGroup(ctx.message.chat.id, [
+                    {
+                        type: type,
+                        media: createUrl.url,
+                        caption: `@smvideosdl_bot`,
+                    },
+                ])
+                .then((res) => {
+                    ctx.telegram.forwardMessage(
+                        '@downloadedVideos',
+                        res[0].chat.id,
+                        res[0].message_id
+                    );
+                });
+        })
+        
     } catch (err) {
         ctx.reply(
             'This link is incorrectly entered or the user account is private',
@@ -77,65 +53,50 @@ const InstagramVideo = async (ctx) => {
 const InstagramStories = async (ctx) => {
     try {
         ctx.reply('⏳');
-        const {data} = await axios.post(
-            'https://reelit.io/api/fetch',
-            {
-                url: ctx.message.text,
-                type: 'story',
-                username: ctx.message.text.split('/')[5]
-            },
-            {
-                headers: {
-                    'User-Agent':
-                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
-                        'X-Requested-With': 'XMLHttpRequest'
-                },
+        const {data} = await axios.get('https://api.instavideosave.com/allinone',{
+            headers:{
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                    'X-Requested-With': 'XMLHttpRequest',
+                url: ctx.message.text
             }
-        );
+        })
+        data.video && data.video.forEach(e => {
+            ctx.replyWithChatAction(`upload_video`);
+            ctx.telegram
+                .sendMediaGroup(ctx.message.chat.id, [
+                    {
+                        type: 'video',
+                        media: e.video,
+                        caption: `@smvideosdl_bot`,
+                    },
+                ])
+                .then((res) => {
+                    ctx.telegram.forwardMessage(
+                        '@downloadedVideos',
+                        res[0].chat.id,
+                        res[0].message_id
+                    );
+                });
+        })
 
-        data.media.data.stories.forEach(e => {
-            var videos = e.videos ? e.videos : []
-            var images = e.images ? e.images : []
-            var username = data.media.data.user ? data.media.data.user.username : `Instagramdan ko'rish`
-            if(videos.length){
-                ctx.replyWithChatAction(`upload_video`);
-                ctx.telegram
-                    .sendMediaGroup(ctx.message.chat.id, [
-                        {
-                            type: 'video',
-                            media: videos[0].url,
-                            parse_mode: 'HTML',
-                            caption: `
-                            @smvideosdl_bot\n\n<i><a href="${ctx.message.text}">${username}</a></i>
-                            `,
-                        },
-                    ])
-                    .then((res) => {
-                        ctx.telegram.forwardMessage(
-                            '@downloadedVideos',
-                            res[0].chat.id,
-                            res[0].message_id
-                        );
-                    });
-            } else if(!videos.length && images) {
-                console.log(e);
-                ctx.replyWithChatAction(`upload_photo`);
-                ctx.telegram
-                    .sendMediaGroup(ctx.message.chat.id, [
-                        {
-                            type: 'photo',
-                            media: e.images[2].url,
-                            caption: `@smvideosdl_bot`,
-                        },
-                    ])
-                    .then((res) => {
-                        ctx.telegram.forwardMessage(
-                            '@downloadedVideos',
-                            res[0].chat.id,
-                            res[0].message_id
-                        );
-                    });
-            }
+        data.image && data.image.forEach(e => {
+            ctx.replyWithChatAction(`upload_photo`);
+            ctx.telegram
+                .sendMediaGroup(ctx.message.chat.id, [
+                    {
+                        type: 'photo',
+                        media: e,
+                        caption: `@smvideosdl_bot`,
+                    },
+                ])
+                .then((res) => {
+                    ctx.telegram.forwardMessage(
+                        '@downloadedVideos',
+                        res[0].chat.id,
+                        res[0].message_id
+                    );
+                });
         })
         
     } catch (err) {
@@ -151,33 +112,35 @@ const InstagramStories = async (ctx) => {
 
 const ProfilePhoto = async (ctx) => {
     try {
-        ctx.reply('⏳');
-        const username = ctx.message.text.slice(1)
-        const { data } = await axios.get('https://storiesig.info/api/ig/profile/' + username, {
-            headers: {
-                'referer': 'https://storiesig.info/en/instagram-story-downloader/',
-                'user-agent':
-                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-                'x-token': null,
-                'x-xsrf-token': process.env.XTOKEN
-            },
-        })
-        ctx.replyWithChatAction('upload_photo');
-        ctx.telegram
-            .sendMediaGroup(ctx.message.chat.id, [
-                {
-                    type: 'photo',
-                    media: data.result.profile_pic_url,
-                    caption: `@smvideosdl_bot`,
-                },
-            ])
-            .then((res) => {
-                ctx.telegram.forwardMessage(
-                    '@downloadedVideos',
-                    res[0].chat.id,
-                    res[0].message_id
-                );
-            });
+        ctx.reply('Try again later');
+        // const username = ctx.message.text.slice(1)
+        // ipp(username).then(user => {
+        //     console.log(user);
+        // })
+        // const { data } = await axios.post('https://givefastlink.com/instagram-downloader/api/instagram',{
+        //     url: username,
+        // }, {
+        //     headers: {
+        //         'user-agent':
+        //             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        //     },
+        // })
+        // ctx.replyWithChatAction('upload_photo');
+        // ctx.telegram
+        //     .sendMediaGroup(ctx.message.chat.id, [
+        //         {
+        //             type: 'photo',
+        //             media: data.result.profile_pic_url,
+        //             caption: `@smvideosdl_bot`,
+        //         },
+        //     ])
+        //     .then((res) => {
+        //         ctx.telegram.forwardMessage(
+        //             '@downloadedVideos',
+        //             res[0].chat.id,
+        //             res[0].message_id
+        //         );
+        //     });
     } catch (err) {
         console.log(err.message);
     }
